abc9ffa66a67a877e56d1c7444d1cae1080a05b0
Sebastian Quack <Sebastian Quack>
2019-07-30 12:21:05 +0200

    circles graph

--- client/src/components/SimpleGraph.js
+++ client/src/components/SimpleGraph.js
@@ -38,7 +38,7 @@ class SimpleGraph extends Component {
     const nodes = data.nodes.map(d => Object.create(d));
 
     const width = element.offsetWidth;
-    const height = 300;
+    const height = 400;
 
     const simulation = d3.forceSimulation(nodes)
         .force("link", d3.forceLink(links).id(d => d.id))
@@ -51,36 +51,51 @@ class SimpleGraph extends Component {
       .append("svg")
         .attr("viewBox", [-width / 2, -height / 2, width, height]);
       
+    const nodeCircles = svg.append("g")
+      .selectAll("g")
+      .data(nodes)
+      .join("g")    
+    
+    let circles = [];
+    for(let i = 0; i < 5; i++) {
+      circles[i] = nodeCircles.append("circle")
+        .attr("stroke", "#eee")      
+        .attr("fill", "none")
+        .attr("r", d => d.weight + Math.pow(i + 1, 3))
+    }
+
+    const nodeColor = "#69b3a2";
+
     const link = svg.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
+        .attr("stroke", nodeColor)
+        .attr("stroke-opacity", 0.2)
       .selectAll("line")
       .data(links)
       .join("line")
-        .attr("stroke-width", d => Math.sqrt(d.value));
+        .attr("stroke-width", 1);
 
     const nodeContainer = svg.append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.5)
+        .attr("stroke", "#eee")
+        .attr("stroke-width", 1)
       .selectAll("g")
       .data(nodes)
       .join("g")
 
     const node = nodeContainer.append("circle")
-          .attr("r", d => 4 + d.weight)
-          .attr("fill", d => "#eee")
+          .attr("stroke", "#69b3a2")
+          .attr("fill", "#fff")
+          .attr("r", d => d.weight)
           .call(this.drag(simulation));
 
-    node.append("title")
-        .text(d => d.id);
-
-    const label = nodeContainer.append("text")
-        .attr("class", "node-label")
+    const label = svg.append("g")
         .attr("dx", 12)
         .attr("dy", ".35em")
         .attr("stroke", "#666")
         .attr("stroke-width", 0.1)
         .attr("font-size", 10)
+        .selectAll("text")
+        .data(nodes)
+        .join("text")
         .text(d => d.id);
 
     simulation.on("tick", () => {
@@ -94,9 +109,15 @@ class SimpleGraph extends Component {
           .attr("cx", d => d.x)
           .attr("cy", d => d.y);
 
+      for(let i = 0; i < 5; i++) {
+        circles[i]
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+      }
+
       label
-          .attr("x", d => d.x)
-          .attr("y", d => d.y);
+          .attr("x", d => d.x + 7 + d.weight)
+          .attr("y", d => d.y + 3);
 
     });
 
