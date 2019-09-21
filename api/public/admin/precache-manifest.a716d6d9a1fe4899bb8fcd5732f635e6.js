436e88b985a3bce26013416bdfff4ecc45487c98
Sebastian Quack <Sebastian Quack>
2019-09-21 12:09:14 +0200

    deploy

---
+++ api/public/admin/precache-manifest.a716d6d9a1fe4899bb8fcd5732f635e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f2ff30e113309591518b6c50af689a",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "b6f241611f93cb83048a",
+    "url": "/admin/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4e33c0ce6d39d7633850",
+    "url": "/admin/static/js/2.2b7f4e8d.chunk.js"
+  },
+  {
+    "revision": "b6f241611f93cb83048a",
+    "url": "/admin/static/js/main.6df55998.chunk.js"
+  },
+  {
+    "revision": "7c4f45329e47798217a3",
+    "url": "/admin/static/js/runtime~main.b3695f6e.js"
+  }
+]);
---
+++ api/public/client/precache-manifest.12f6f44c63535c687da4d3049ec988dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17043d18bd69f25e82d7bd792a366488",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "cea48ded8d18dd17b6eb",
+    "url": "/client/static/css/2.331648e1.chunk.css"
+  },
+  {
+    "revision": "e6e8a842613ada112fb4",
+    "url": "/client/static/css/main.9fdfee58.chunk.css"
+  },
+  {
+    "revision": "cea48ded8d18dd17b6eb",
+    "url": "/client/static/js/2.53a1ab63.chunk.js"
+  },
+  {
+    "revision": "e6e8a842613ada112fb4",
+    "url": "/client/static/js/main.5ca3f45b.chunk.js"
+  },
+  {
+    "revision": "79f1a12bb5d1cdf91f65",
+    "url": "/client/static/js/runtime~main.3fca2621.js"
+  }
+]);
