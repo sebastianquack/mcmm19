d650c9cbd23be64a5d67b5375641fb356b26bac5
Sebastian Quack <Sebastian Quack>
2019-09-27 09:51:43 +0200

    client production build

---
+++ api/public/client/precache-manifest.d56503e925dd0c3b2ccb9ef6486a8591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de089c31207363d034a57de7ddf9c26f",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "85a545b4e9d638c20a4e",
+    "url": "/client/static/css/2.331648e1.chunk.css"
+  },
+  {
+    "revision": "606a2692eb6b9637cbb9",
+    "url": "/client/static/css/main.cc5540d8.chunk.css"
+  },
+  {
+    "revision": "85a545b4e9d638c20a4e",
+    "url": "/client/static/js/2.c18982c3.chunk.js"
+  },
+  {
+    "revision": "606a2692eb6b9637cbb9",
+    "url": "/client/static/js/main.b9d2edca.chunk.js"
+  },
+  {
+    "revision": "79f1a12bb5d1cdf91f65",
+    "url": "/client/static/js/runtime~main.3fca2621.js"
+  }
+]);
